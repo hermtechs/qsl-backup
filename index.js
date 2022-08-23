8465c299cfb36e553e2c99dff3d5895d5f1e8ac3
hermtechs <hermtechs@gmail.com>
2022-08-23 12:48:02 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const navBtn = document.querySelector('.hamburger');
+const smallScreenMenu = document.querySelector('.nav-links-small-screen');
+const navLinksSmall = smallScreenMenu.querySelectorAll('a');
+
+const scrollRightBtn = document.querySelector('.scroll-left')
+const scrollLeftBtn = document.querySelector('.scroll-right')
+const slideShowContainer = document.querySelector('.services-slide-show')
+const mainDataContainer = document.querySelector('.user-data-section');
+//navigation menu toggle
+navBtn.addEventListener('click', openAndCloseMenu)
+function openAndCloseMenu(){  
+if(navBtn.classList.contains('nav-open')){
+    navBtn.classList.remove('nav-open');
+    smallScreenMenu.style.transform = 'translateX(-100%)'
+}
+else{
+    navBtn.classList.add('nav-open');
+    smallScreenMenu.style.transform = 'translateX(0%)'
+}
+}
+
+    navLinksSmall.forEach(navLink=>{
+        navLink.addEventListener('click',openAndCloseMenu)
+    })
