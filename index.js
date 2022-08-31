b51e5e99f8a7f8eb21f3e20e6fe797b6292a985a
joseph-winner <muhanguzijoseph2021@gmail.com>
2022-08-31 07:26:15 +0300

    updates on qsl

--- index.js
+++ index.js
@@ -67,23 +67,64 @@ setInterval(countDown, 1000);
 
 
 /* SLIDER IMAGES WITH JAVASCRIPT AND CSS STARTS HERE */
-var slider = document.querySelector(".banner").children,
-  sliderLen = slider.length;
+const slider = document.querySelector(".slides").children;
+const slides = slider.length
+const prev = document.querySelector(".prev")
+const next = document.querySelector(".next")
+const duration = 6000;
 
-let slid = 0;
+let index = 0;
 
-function slides() {
-  for (let i = 0; i < sliderLen; i++) {
-    slider[i].classList.remove("slide-play");
-  }
-  slider[slid].classList.add("slide-play");
-  if (slid == sliderLen - 1) {
-    slid = 0;
-  } else {
-    slid++;
-  }
-  setTimeout(slides, 5000);
+prev.onclick = function(){
+    slide("prev")
 }
 
-window.onload = slides();
+next.onclick = function(){
+    slide("next")
+}
+
+function slide(direction){
+    // progress()
+    if(direction == 'next'){
+        if(index==slides-1){
+            index=0;
+        }
+        else{
+            index++;
+        }
+    }
+    if(index == 'prev'){
+        if(index == 0){
+            index = slides-1
+        }
+        else{
+            index--;
+        }
+    }
+    for(let i = 0; i<slides; i++){
+        slider[i].classList.remove("activeSlide")
+    }
+    slider[index].classList.add("activeSlide");
+}
+
+
+function progress(){
+    document.querySelector(".meter").innerHTML = '';
+    const div = document.createElement("div");
+    div.style.height = "5px";
+    div.style.width = "0px";
+    div.style.position = "absolute";
+    div.style.left = "0px";
+    div.style.top = "0px";
+    div.style.backgroundColor = "blue";
+    div.id = "meter";
+    div.style.animation = "progress "+ duration/1000 + "s linear";
+    document.querySelector(".meter").appendChild(div);
+}
+// progress()
+
+function autoslide(){
+    slide("next");
+}
+let timer = setInterval(autoslide, duration);
 /* SLIDER IMAGES WITH JAVASCRIPT AND CSS ENDS HERE */
